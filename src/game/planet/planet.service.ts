eb35c589b52d31841e90e17a1e046bb984c435b9
LeartAule <kirill.kondrashow@mail.ru>
2022-01-15 21:53:38 +0300

    fixed response (planet.service)

--- src/game/planet/planet.service.ts
+++ src/game/planet/planet.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@nestjs/common';
 import {InjectModel} from '@nestjs/sequelize';
 import {Planet} from "./planet.model";
 import {Race} from "../race/race.model";
+import {Politics} from "./politics/politics.model";
+import {Economics} from "./economics/economics.model";
 
 
 @Injectable()
@@ -10,20 +12,21 @@ export class PlanetService {
     constructor(@InjectModel(Planet) private planetRepository: typeof Planet, @InjectModel(Race) private raceRepository: typeof Race) {}
 
     async getAll(){
-        return await this.planetRepository.findAll({include: {all: true}})
+        return await this.planetRepository.findAll({include: [{model: Politics}, {model: Economics}]})
     }
 
     findOneById(id: string): Promise<Planet>{
         return this.planetRepository.findOne({
+
             where:{
                 id,
             },
-            include:{all:true}
+            include:[{model: Politics}, {model: Economics}, {model: Race}]
         })
     }
 
     async getAllBySystemId(systemId: string){
-        return await this.planetRepository.findAll({where: {system_id: systemId, }, include: {all: true}})
+        return await this.planetRepository.findAll({where: {system_id: systemId, }, include: [{model: Politics}, {model: Economics}, {model: Race}]})
     }
 
     async removeOne(id: string): Promise<void>{
